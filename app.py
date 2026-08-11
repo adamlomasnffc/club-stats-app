85b05e14f311c55591e8f756c96f57cfdae3f4d5
adamlomasnffc <adamlomas87@gmail.com>
2026-08-11 23:40:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ VIDEO_URL = "https://raw.githubusercontent.com/adamlomasnffc/club-stats-app/main
 
 st.set_page_config(
     page_title="Derby Penguins App",
-    page_icon=LOGO_URL,
+    page_icon=LOGO_URLv2,
     layout="wide"
 )
 
