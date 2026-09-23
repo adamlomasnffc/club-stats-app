bb25bdca40c7b838e937c9eca3cfb02f4cddb997
adamlomasnffc <adamlomas87@gmail.com>
2026-09-23 21:42:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -254,6 +254,8 @@ def render_page_header(title, img_url=None, invert=False):
             f"<h2 style='text-align: center; margin-bottom: 12px;'>{title}</h2>"
         )
 
+if st.sidebar.checkbox("🔧 Show debug page"):
+    current_page = "Debug"
 
 pages_config = [
     ("🏠 Home", "Homepage", None),
